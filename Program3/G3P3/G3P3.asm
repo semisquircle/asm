6799e64b1ab416f82b7b60f6d70777d654ecd370
semisquircle <semisquircle@gmail.com>
2026-03-24 00:29:11 -0400

    variable names fix + command handler fix

--- Program3/G3P3/G3P3.asm
+++ Program3/G3P3/G3P3.asm
@@ -8,22 +8,23 @@
 TITLE G3P3
 INCLUDE Irvine32.inc
 
-JName EQU 0
-JPriority EQU 8
-JStatus EQU 9
-JRunTime EQU 10
-JLoadTime EQU 12
+JName equ 0
+JPriority equ 8
+JStatus equ 9
+JRunTime equ 10
+JLoadTime equ 12
 
-JobAvailable EQU 0
-JobRun EQU 1
-JobHold EQU 2
+JobAvailable equ 0
+JobRun equ 1
+JobHold equ 2
 
 LowestPriority equ 7
-SizeOfJob EQU 14
-NumberOfJobs EQU 10
+SizeOfJob equ 14
+NumberOfJobs equ 10
 
 .data
-Jobs BYTE NumberOfJobs * SizeOfJob DUP(JobAvailable)
+jobs BYTE NumberOfJobs * SizeOfJob DUP(0)
+endOfJobs DWORD endOfJobs
 
 jobCount DWORD 0
 sysTime DWORD 0
@@ -34,14 +35,14 @@ cmdQuit BYTE "QUIT", 0
 cmdHelp BYTE "HELP", 0
 cmdShow BYTE "SHOW", 0
 cmdLoad BYTE "LOAD", 0
-cmdRun  BYTE "RUN", 0
+cmdRun BYTE "RUN", 0
 cmdHold BYTE "HOLD", 0
 cmdKill BYTE "KILL", 0
 cmdStep BYTE "STEP", 0
 cmdChange BYTE "CHANGE", 0
 
 msgHelp BYTE "Commands: LOAD RUN HOLD KILL SHOW STEP CHANGE QUIT", 0
-msgPrompt BYTE ">> ", 0
+msgPrompt BYTE "Enter a command: ", 0
 msgInvalid BYTE "Invalid command", 0
 msgFull BYTE "Job queue full!", 0
 msgExists BYTE "Job already exists!", 0
@@ -51,9 +52,11 @@ msgLoaded BYTE "Job loaded", 0
 msgRunning BYTE "Job running", 0
 msgHolding BYTE "Job holding", 0
 msgDone BYTE "Job completed", 0
+msgChanged BYTE "Job priority changed", 0
 
 .code
 main PROC
+	cld
 mainLoop:
 	mov edx, OFFSET msgPrompt
 	call WriteString
@@ -61,104 +64,216 @@ mainLoop:
 	mov edx, OFFSET buffer
 	mov ecx, 64
 	call ReadString
+
+	call ToUpperBuffer
 	call ParseCommand
 
 	jmp mainLoop
 main ENDP
 
-SkipSpaces PROC
+ToUpperBuffer PROC
+	mov edi, OFFSET buffer
+UpperLoop:
+	mov al, [edi]
+	cmp al, 0
+	je done
+
+	cmp al, 'a'
+	jb next
+	cmp al, 'z'
+	ja next
+
+	sub al, 32
+	mov [edi], al
+
+next:
+	inc edi
+	jmp UpperLoop
+done:
+	ret
+ToUpperBuffer ENDP
+
+SkipWhitespace PROC
+SkipLoop:
 	mov al, [edi]
 	cmp al, ' '
 	jne done
 	inc edi
-	jmp SkipSpaces
+	jmp SkipLoop
 done:
 	ret
-SkipSpaces ENDP
+SkipWhitespace ENDP
+
+SkipToken PROC
+SkipLoop:
+	mov al, [edi]
+	cmp al, ' '
+	je done
+	cmp al, 0
+	je done
+	inc edi
+	jmp SkipLoop
+done:
+	ret
+SkipToken ENDP
+
+GetTokenPtr PROC
+	call SkipToken
+	call SkipWhitespace
+	mov esi, edi
+	ret
+GetTokenPtr ENDP
+
+ParseNumber PROC
+	xor eax, eax
+ParseLoop:
+	mov bl, [esi]
+	cmp bl, '0'
+	jb done
+	cmp bl, '9'
+	ja done
+
+	sub bl, '0'
+	imul eax, 10
+	add eax, ebx
+
+	inc esi
+	jmp ParseLoop
+done:
+	ret
+ParseNumber ENDP
+
+CopyNameFromBuffer PROC
+	push edi
+	mov ecx, 8
+CopyLoop:
+	mov al, [esi]
+	cmp al, ' '
+	je done
+	cmp al, 0
+	je done
+
+	mov [edi + JName], al
+	inc esi
+	inc edi
+	loop CopyLoop
+done:
+	mov BYTE PTR [edi], 0
+	pop edi
+	ret
+CopyNameFromBuffer ENDP
+
+FindJobByName PROC
+	mov ecx, jobCount
+	mov edi, 0
+
+searchLoop:
+	cmp edi, ecx
+	jge notFound
+
+	mov eax, edi
+	imul eax, SizeOfJob
+	mov edx, OFFSET jobs
+	add edx, eax
+
+	push esi
+	push edi
+
+	mov edi, edx
+	add edi, JName
+
+	mov ecx, 8
+	repe cmpsb
+
+	pop edi
+	pop esi
+
+	je found
+
+	inc edi
+	jmp searchLoop
+
+found:
+	mov eax, edi
+	ret
+
+notFound:
+	mov eax, -1
+	ret
+FindJobByName ENDP
 
 ParseCommand PROC
 	mov edi, OFFSET buffer
-    call SkipSpaces
-    cld
+	call SkipWhitespace
+	cld
 
-    push edi   ; save starting position
+	mov esi, OFFSET cmdQuit
+	mov ecx, SIZEOF cmdQuit
+	push edi
+	repe cmpsb
+	pop edi
+	je DoQuit
 
-    ; ---- QUIT ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdQuit
-    mov ecx, SIZEOF cmdQuit
-    repe cmpsb
-    je DoQuit
+	mov esi, OFFSET cmdHelp
+	mov ecx, SIZEOF cmdHelp
+	push edi
+	repe cmpsb
+	pop edi
+	je DoHelp
 
-    ; ---- HELP ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdHelp
-    mov ecx, SIZEOF cmdHelp
-    repe cmpsb
-    je DoHelp
+	mov esi, OFFSET cmdShow
+	mov ecx, SIZEOF cmdShow
+	push edi
+	repe cmpsb
+	pop edi
+	je DoShow
 
-    ; ---- SHOW ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdShow
-    mov ecx, SIZEOF cmdShow
-    repe cmpsb
-    je DoShow
+	mov esi, OFFSET cmdLoad
+	mov ecx, SIZEOF cmdLoad
+	push edi
+	repe cmpsb
+	pop edi
+	je DoLoad
 
-    ; ---- LOAD ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdLoad
-    mov ecx, SIZEOF cmdLoad
-    repe cmpsb
-    je DoLoad
+	mov esi, OFFSET cmdRun
+	mov ecx, SIZEOF cmdRun
+	push edi
+	repe cmpsb
+	pop edi
+	je DoRun
 
-    ; ---- RUN ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdRun
-    mov ecx, SIZEOF cmdRun
-    repe cmpsb
-    je DoRun
+	mov esi, OFFSET cmdHold
+	mov ecx, SIZEOF cmdHold
+	push edi
+	repe cmpsb
+	pop edi
+	je DoHold
 
-    ; ---- HOLD ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdHold
-    mov ecx, SIZEOF cmdHold
-    repe cmpsb
-    je DoHold
+	mov esi, OFFSET cmdKill
+	mov ecx, SIZEOF cmdKill
+	push edi
+	repe cmpsb
+	pop edi
+	je DoKill
 
-    ; ---- KILL ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdKill
-    mov ecx, SIZEOF cmdKill
-    repe cmpsb
-    je DoKill
+	mov esi, OFFSET cmdStep
+	mov ecx, SIZEOF cmdStep
+	push edi
+	repe cmpsb
+	pop edi
+	je DoStep
 
-    ; ---- STEP ----
-    pop edi
-    push edi
-    mov esi, OFFSET cmdStep
-    mov ecx, SIZEOF cmdStep
-    repe cmpsb
-    je DoStep
+	mov esi, OFFSET cmdChange
+	mov ecx, SIZEOF cmdChange
+	push edi
+	repe cmpsb
+	pop edi
+	je DoChange
 
-    ; ---- CHANGE ----
-    pop edi
-    mov esi, OFFSET cmdChange
-    mov ecx, SIZEOF cmdChange
-    repe cmpsb
-    je DoChange
-
-    ; ---- INVALID ----
-    mov edx, OFFSET msgInvalid
-    call WriteString
-    call Crlf
-    ret
+	mov edx, OFFSET msgInvalid
+	call WriteString
+	call Crlf
+	ret
 
 DoQuit:
 	exit
@@ -181,10 +296,43 @@ DoKill:
 	call KillJob
 	ret
 DoStep:
-	call StepSystem
+	mov ecx, 1
+	call GetTokenPtr
+	cmp BYTE PTR [esi], 0
+	je DoStepLoop
+	call ParseNumber
+	mov ecx, eax
+DoStepLoop:
+	mov edx, ecx
+StepLoop:
+	call ProcessNextJob
+	inc sysTime
+	dec edx
+	jnz StepLoop
 	ret
 DoChange:
-	call ChangePriority
+	call GetTokenPtr
+	call FindJobByName
+	cmp eax, -1
+	je notFoundChange
+
+	imul eax, SizeOfJob
+	mov edi, OFFSET jobs
+	add edi, eax
+
+	call GetTokenPtr
+	call ParseNumber
+	mov al, al
+	mov JPriority[edi], al
+
+	mov edx, OFFSET msgChanged
+	call WriteString
+	call Crlf
+	ret
+notFoundChange:
+	mov edx, OFFSET msgNotFound
+	call WriteString
+	call Crlf
 	ret
 ParseCommand ENDP
 
@@ -192,22 +340,24 @@ LoadJob PROC
 	cmp jobCount, NumberOfJobs
 	jge fullQueue
 
-	call GetJobName ; NEEDS FIXING
-	call FindJob ; NEEDS FIXING
+	call GetTokenPtr
+	call FindJobByName
 	cmp eax, -1
 	jne exists
 
 	mov ebx, jobCount
 	imul ebx, SizeOfJob
-	mov edi, OFFSET Jobs
+	mov edi, OFFSET jobs
 	add edi, ebx
 
-	call CopyName ; NEEDS FIXING
+	call CopyNameFromBuffer
 
-	call GetNumber ; NEEDS FIXING
+	call GetTokenPtr
+	call ParseNumber
 	mov JPriority[edi], al
 
-	call GetNumber ; NEEDS FIXING
+	call GetTokenPtr
+	call ParseNumber
 	mov JRunTime[edi], al
 
 	mov BYTE PTR JStatus[edi], JobHold
@@ -236,12 +386,13 @@ exists:
 LoadJob ENDP
 
 RunJob PROC
-	call FindJobByInput ; NEEDS FIXING
+	call GetTokenPtr
+	call FindJobByName
 	cmp eax, -1
 	je notFound
 
 	imul eax, SizeOfJob
-	mov edi, OFFSET Jobs
+	mov edi, OFFSET jobs
 	add edi, eax
 
 	mov BYTE PTR JStatus[edi], JobRun
@@ -259,12 +410,13 @@ notFound:
 RunJob ENDP
 
 HoldJob PROC
-	call FindJobByInput ; NEEDS FIXING
+	call GetTokenPtr
+	call FindJobByName
 	cmp eax, -1
 	je notFound
 
 	imul eax, SizeOfJob
-	mov edi, OFFSET Jobs
+	mov edi, OFFSET jobs
 	add edi, eax
 
 	mov BYTE PTR JStatus[edi], JobHold
@@ -276,9 +428,14 @@ HoldJob PROC
 HoldJob ENDP
 
 KillJob PROC
-	call FindJobByInput ; NEEDS FIXING
+	call GetTokenPtr
+	call FindJobByName
 	cmp eax, -1
-	je notFound
+	je notFoundKill
+
+	imul eax, SizeOfJob
+	mov esi, OFFSET jobs
+	add esi, eax
 
 	call RemoveJobIndex
 
@@ -286,15 +443,47 @@ KillJob PROC
 	call WriteString
 	call Crlf
 	ret
+
+notFoundKill:
+	mov edx, OFFSET msgNotFound
+	call WriteString
+	call Crlf
+	ret
 KillJob ENDP
+
+RemoveJobIndex PROC
+	mov ecx, jobCount
+	cmp ecx, 0
+	je doneRemove
+
+	mov edi, esi
+nextShift:
+	add edi, SizeOfJob
+	cmp edi, OFFSET jobs + (jobCount-1)*SizeOfJob
+	jae doneShift
+
+	mov eax, SizeOfJob
+	mov esi, edi
+	sub esi, SizeOfJob
+	mov edx, OFFSET jobs
+	add edx, esi
+
+	mov ecx, SizeOfJob
+	rep movsb
+
+	jmp nextShift
+doneShift:
+	dec jobCount
+doneRemove:
+	ret
+RemoveJobIndex ENDP
 
 StepSystem PROC
 	mov ecx, 1
-
-stepLoop:
+StepLoop:
 	call ProcessNextJob
 	inc sysTime
-	loop stepLoop
+	loop StepLoop
 	ret
 StepSystem ENDP
 
@@ -310,7 +499,7 @@ findLoop:
 
 	mov eax, esi
 	imul eax, SizeOfJob
-	mov edx, OFFSET Jobs
+	mov edx, OFFSET jobs
 	add edx, eax
 
 	cmp BYTE PTR JStatus[edx], JobRun
@@ -332,7 +521,7 @@ done:
 	je noJob
 
 	imul edi, SizeOfJob
-	mov edx, OFFSET Jobs
+	mov edx, OFFSET jobs
 	add edx, edi
 
 	dec BYTE PTR JRunTime[edx]
@@ -357,28 +546,23 @@ ShowJobs PROC
 
 showLoop:
 	cmp esi, ecx
-	jge done
+	jge doneShow
 
 	mov eax, esi
 	imul eax, SizeOfJob
-	mov edx, OFFSET Jobs
+	mov edx, OFFSET jobs
 	add edx, eax
 
-	lea edx, JName[edx] ; NEEDS FIXING, just weird to use lea
+	add edx, JName
 	call WriteString
 	call Crlf
 
 	inc esi
 	jmp showLoop
 
-done:
+doneShow:
 	ret
 ShowJobs ENDP
-
-RemoveJobIndex PROC
-	dec jobCount
-	ret
-RemoveJobIndex ENDP
 
 ShowHelp PROC
 	mov edx, OFFSET msgHelp
